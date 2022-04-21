e85e4d64f7c734ebd88695cd308000d3c164af1c
x00Pavel <pavel.yadlouski@gmail.com>
2022-04-21 12:06:54 +0200

    Add unit tests for IPA server CA

--- test/test_ca.py
+++ test/test_ca.py
@@ -1,11 +1,48 @@
 import pytest
 import re
 from pathlib import Path
+from python_freeipa.client_meta import ClientMeta
 from shutil import copyfile
 from subprocess import check_output
 
 from SCAutolib import TEMPLATES_DIR
-from SCAutolib.models import local_ca
+from SCAutolib.models import local_ca, ipa_server
+
+
+@pytest.fixture()
+def dummy_ipa_vals(ipa_ip, ipa_hostname, ipa_admin_passwd, ipa_root_passwd):
+    """
+    Creates dummy values for IPA serve and client for testings
+    """
+    domain = ipa_hostname.split(".", 1)[1]
+    return {
+        "server_ip": ipa_ip,
+        "server_domain": domain,
+        "server_hostname": ipa_hostname,
+        "server_admin_passwd": ipa_admin_passwd,
+        "server_realm": domain.upper(),
+        "server_root_passwd": ipa_root_passwd,
+        "client_hostname": f"client-hostname.{domain}"
+    }
+
+
+@pytest.fixture()
+def ipa_meta_client(dummy_ipa_vals):
+    """
+    Return ready-to-use IPA MetaClient with admin login. This fixture might not
+    work if there is no mapping rule on your system for given IPA IP address and
+    IPA hostnames (no corresponding entry in /etc/hosts)
+    """
+    client = ClientMeta(dummy_ipa_vals["server_hostname"], verify_ssl=False)
+    client.login("admin", dummy_ipa_vals["server_admin_passwd"])
+    return client
+
+
+@pytest.fixture()
+def clean_ipa():
+    yield
+    check_output(["ipa-client-install", "--uninstall", "--unattended"],
+                 encoding="utf-8")
 
 
 def test_local_ca_setup(backup_sssd_ca_db, tmpdir, caplog):
@@ -85,3 +122,24 @@ def test_revoke_cert(local_ca_fixture, tmpdir):
 
     with open(Path(local_ca_fixture.root_dir, "index.txt"), "r") as f:
         assert re.match(rex, f.read())
+
+
+def test_ipa_server_simple_setup(ipa_meta_client, dummy_ipa_vals, clean_ipa,
+                                 caplog):
+    ipa_ca = ipa_server.IPAServerCA(ip_addr=dummy_ipa_vals["server_ip"],
+                                    client_hostname=dummy_ipa_vals[
+                                        "client_hostname"],
+                                    hostname=dummy_ipa_vals["server_hostname"],
+                                    root_passwd=dummy_ipa_vals[
+                                        "server_root_passwd"],
+                                    admin_passwd=dummy_ipa_vals[
+                                        "server_admin_passwd"],
+                                    domain=dummy_ipa_vals["server_domain"])
+
+    ipa_ca.setup()
+
+    # Test if meta client can get info about freshly configured host
+    ipa_meta_client.host_show(a_fqdn=dummy_ipa_vals["client_hostname"])
+
+    # Cleanup after test
+    ipa_meta_client.host_del(a_fqdn=dummy_ipa_vals["client_hostname"])
